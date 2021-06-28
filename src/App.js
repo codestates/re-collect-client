f1592601ce71072227dd09ce47e229600ec6a5f4
CodingGorani <tjr2922@gmail.com>
2021-06-28 14:57:20 +0900

    [client]done/initial setting

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import "./App.css";
+import './App.css';
+import { Nav, Footer } from './components';
+import { Link, Switch, Route, Redirect, withRouter } from 'react-router-dom';
 
 function App() {
-  return <div className="App">Hi</div>;
+  return;
+  <Router>
+    <Nav />
+    <Switch>
+      <Route path="/collect" />
+      <Route path="/profile" />
+      <Route path="/explore" />
+      <Route exact path="/" />
+    </Switch>
+    <Footer />
+  </Router>;
 }
 
-export default App;
+export default withRouter(App);
