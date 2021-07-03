63d13ee0a92efdf61206cf06c3475b934f645775
Siyooon <ksyoon0718@gmail.com>
2021-07-03 16:35:01 +0900

    [client]error-handling

--- src/App.js
+++ src/App.js
@@ -1,33 +1,6 @@
-import './App.css';
-<<<<<<< HEAD
-// import { Nav, Footer } from './components';
-import Nav from './components/Nav';
-import Footer from './components/Footer';
-import Collect from './page/Collect';
-import {
-  BrowserRouter as Router,
-  Link,
-  Switch,
-  Route,
-  Redirect,
-  withRouter,
-} from 'react-router-dom';
 
-function App() {
-  return (
-    <Router>
-      <Nav />
-      <Switch>
-        <Route path="/collect" render={() => <Collect />} />
-        <Route path="/profile" />
-        <Route path="/explore" />
-        <Route exact path="/" />
-      </Switch>
-      <Footer />
-    </Router>
-  );
-=======
 import React from 'react';
+import Collect from './page/Collect';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Landing from './page/Landing';
@@ -39,7 +12,7 @@ function App() {
     <Router>
     <Nav />
     <Switch>
-      <Route path="/collect"></Route>
+    <Route path="/collect" render={() => <Collect />} />
       <Route path="/profile"></Route>
       <Route path="/explore"></Route>
       <Route exact path="/" component={Landing}></Route>
@@ -50,7 +23,6 @@ function App() {
     
   )
 
->>>>>>> fdb03d0465d30362122e7f3a0eb237a8498786e8
 }
 
 export default withRouter(App);
