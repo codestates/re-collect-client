c12f1939f0a7cfe10910aa7b8518d4200044cebe
CodingGorani <tjr2922@gmail.com>
2021-07-03 17:48:43 +0900

    [client]done/css

---
+++ src/page/Loading.js
@@ -0,0 +1,9 @@
+function Loading() {
+  return (
+    <div className="loading">
+      loading...
+      <div className="loading__logo">logo</div>
+      <div className="loading__letter">Recollect</div>
+    </div>
+  );
+}
