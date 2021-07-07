ee20a77a159f42caf902e8dab4473f9f72def2c2
CodingGorani <tjr2922@gmail.com>
2021-07-07 16:22:29 +0900

    [client] bugFix/loginModal.js

--- src/components/LoginModal.js
+++ src/components/LoginModal.js
@@ -1,26 +1,26 @@
-import React, { useEffect } from "react";
+import React, { useEffect } from 'react';
 import {
   IsValidateEmail,
   IsValidiateUsername,
   IsValidiatePassword,
-} from "../util/validation";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faTimes } from "@fortawesome/free-solid-svg-icons";
+} from '../util/validation';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTimes } from '@fortawesome/free-solid-svg-icons';
 
 function LoginModal(props) {
   const loginValidCheck = () => {
-    const email = document.querySelector(".loginEmail").value;
-    const pwd = document.querySelector(".loginPwd").value;
-    const error = document.querySelector(".loginErrorMessage");
+    const email = document.querySelector('.loginEmail').value;
+    const pwd = document.querySelector('.loginPwd').value;
+    const error = document.querySelector('.loginErrorMessage');
     if (!IsValidateEmail(email)) {
-      error.textContent = "이메일을 확인해주세요";
+      error.textContent = '이메일을 확인해주세요';
       return;
     } else if (!IsValidiatePassword(pwd)) {
-      error.textContent = "비밀번호를 확인해주세요";
+      error.textContent = '비밀번호를 확인해주세요';
       return;
     } else {
-      error.textContent = "";
-      props.setLoginMode(!props.loginMode);
+      error.textContent = '';
+      props.setModalMode('');
     }
   };
 
@@ -30,7 +30,7 @@ function LoginModal(props) {
         <div
           className="closeBtn"
           onClick={() => {
-            props.setModalMode("");
+            props.setModalMode('');
           }}
         >
           <FontAwesomeIcon icon={faTimes} />
@@ -52,14 +52,14 @@ function LoginModal(props) {
         <div className="buttonContainer">
           <div
             onClick={() => {
-              props.setModalMode("signup");
+              props.setModalMode('signup');
             }}
           >
             회원가입
           </div>
           <div
             onClick={() => {
-              props.setModalMode("findPwd");
+              props.setModalMode('findPwd');
             }}
           >
             비밀번호 찾기
